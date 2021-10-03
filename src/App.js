693f996e0036a47d6ba4e1777969024e0a3bba71
keroken <kenichi.shinagawa@gmail.com>
2021-10-03 15:26:01 +0900

    move sendCartData to App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import Notification from './components/UI/Notification';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { uiActions } from './store';
+import { sendCartData } from './store/cartSlice';
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
   const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   useEffect(() => {
     if (notification) {
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,12 +1,10 @@
 import classes from './CartItem.module.css';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { cartActions } from '../../store';
-import { sendCartData } from '../../store/cartSlice';
 
 const CartItem = (props) => {
   const { title, quantity, total, price, id } = props.item;
   const dispatch = useDispatch();
-  const cart = useSelector(state => state.cart);
 
   const removeItemHandler = () => {
     dispatch(cartActions.removeItemFromCart(id));
@@ -18,7 +16,6 @@ const CartItem = (props) => {
       title,
       price,
     }));
-    dispatch(sendCartData(cart));
   };
 
   return (
