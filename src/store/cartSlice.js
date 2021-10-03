0eb114083900b7910491d8d8e55d3af6c1b6e65e
keroken <kenichi.shinagawa@gmail.com>
2021-10-03 15:04:31 +0900

    create sendCartData action

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from '.';
+import { database } from '../database/config';
 
 const initialCartState = { items: [], totalQuantity: 0 };
 
@@ -36,5 +38,40 @@ const cartSlice = createSlice({
     },
   }
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending...',
+      message: 'Sending cart data!',
+    });
+
+    const sendRequest = async () => {
+      return database.ref('cart').update({
+        body: JSON.stringify(cart)
+      });
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sending cart data failed.',
+        })
+      );
+    }
+  };
+};
 
 export default cartSlice;
