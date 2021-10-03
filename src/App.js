e0c7b2b2abeb9270b0947bbc9ea14dfe4f07d281
keroken <kenichi.shinagawa@gmail.com>
2021-10-03 15:06:52 +0900

    add notification logic to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Notification from './components/UI/Notification';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { uiActions } from './store';
 
 function App() {
+  const dispatch = useDispatch();
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    if (notification) {
+      setTimeout(() => {
+        dispatch(uiActions.hideNotification());
+      }, 1000);
+    }
+  }, [dispatch, notification]);
+
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
