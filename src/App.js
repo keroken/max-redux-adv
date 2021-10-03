a47972c9b97737c71cfe2676c36df0195ac91c05
keroken <kenichi.shinagawa@gmail.com>
2021-10-03 17:05:22 +0900

    add fetchCartData action to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Notification from './components/UI/Notification';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { uiActions } from './store';
-import { sendCartData } from './store/cartDataActions';
+import { sendCartData, fetchCartData } from './store/cartDataActions';
 
 let isInitial = true;
 
@@ -14,6 +14,10 @@ function App() {
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
   const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if (isInitial) {
